053a202849f1dae5b5adb199908663b6e1099807
dheerajyadav1714 <125873179+dheerajyadav1714@users.noreply.github.com>
2023-02-20 16:51:40 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -99,13 +99,6 @@ tk.Label(window1, textvariable=my_string_var2, font=("Arial", 10)).pack()
 
 
 window1.mainloop()
-if __name__ == '__GUI__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. You
-    # can configure startup instructions by adding `entrypoint` to app.yaml.
-    app.run(host='10.24.19.83', port=8080, debug=True)
-# [END gae_python3_app]
-# [END gae_python38_app]
 
 
 
